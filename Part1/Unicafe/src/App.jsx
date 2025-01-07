c44c125c09678e550c913b862b60c4f9c0e523d4
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-01-07 10:01:50 -0300

    Part 1 Unicafe step 6

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -5,17 +5,27 @@ const Button = ({ text, onClick }) => (
 )
 
 const StatisticsLine = ({ text, value }) => (
-  <p>{text}: {value}</p>
+  <>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  </>
 )
 
 const StatisticsLinePercentage = ({ text, value }) => (
-  <p>{text}: {value}%</p>
+  <>
+    <tr>
+      <td>{text}</td>
+      <td>{value}%</td>
+    </tr>
+  </>
 )
 
-const Statistics = ({stats}) => {
-  
+const Statistics = ({ stats }) => {
+
   const { good, neutral, bad, all, average, positive } = stats
-  
+
   if (!all) {
     return (
       <p>No feedback given</p>
@@ -24,12 +34,16 @@ const Statistics = ({stats}) => {
 
   return (
     <>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={all} />
-      <StatisticsLine text="average" value={average} />
-      <StatisticsLinePercentage text="positive" value={positive} />
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLinePercentage text="positive" value={positive} />
+        </tbody>
+      </table>
     </>
   )
 }
@@ -54,21 +68,21 @@ function App() {
 
   const onGood = () => {
     setAverage((good + 1 - bad) / (all + 1))
-    setPositive((good + 1) / (all + 1))
+    setPositive((good + 1) / (all + 1) * 100)
     setAll(all + 1)
     setGood(good + 1)
   }
 
   const onNeutral = () => {
     setAverage((good - bad) / (all + 1))
-    setPositive((good) / (all + 1))
+    setPositive((good) / (all + 1) * 100)
     setAll(all + 1)
     setNeutral(neutral + 1)
   }
 
   const onBad = () => {
     setAverage((good - (bad + 1)) / (all + 1))
-    setPositive((good) / (all + 1))
+    setPositive((good) / (all + 1) * 100)
     setAll(all + 1)
     setBad(bad + 1)
   }
