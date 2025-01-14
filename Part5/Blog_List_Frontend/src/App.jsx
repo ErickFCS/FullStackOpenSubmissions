fe5d00771c2b96d2157aa4e3da4088c63fa5c8e9
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-01-14 10:19:01 -0300

    Part 5 Blog List Frontend step 4

--- Part5/Blog_List_Frontend/src/App.jsx
+++ Part5/Blog_List_Frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react'
 import Blogs from './components/Blogs'
+import Message from './components/Message'
 import AccountForm from './components/AccountForm'
 import CreateForm from './components/CreateForm'
 import BlogService from './services/blogsService'
 import AccountService from './services/accountService'
+import './index.css'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [user, setUser] = useState({})
+    const [message, setMessage] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         BlogService.getAll().then(blogs =>
@@ -26,16 +30,29 @@ const App = () => {
         const password = event.target.password.value
         const newUser = await AccountService
             .login(username, password)
-            .catch((err) => "")
+            .catch((err) => ({}))
         if (newUser.name) {
             setUser(newUser)
             window.localStorage.setItem("user", JSON.stringify(newUser))
+            setMessage("login successful")
+            setTimeout(() => {
+                setMessage(null)
+            }, 3000)
+        } else {
+            setError("login unsuccessful")
+            setTimeout(() => {
+                setError(null)
+            }, 3000)
         }
     }
 
     const logoutHandler = () => {
         setUser({})
         window.localStorage.removeItem("user")
+        setMessage("logout successful")
+        setTimeout(() => {
+            setMessage(null)
+        }, 3000)
     }
 
     const createHandler = async (event) => {
@@ -43,17 +60,31 @@ const App = () => {
         const title = event.target.title.value
         const author = event.target.author.value
         const url = event.target.url.value
-        await BlogService.createBlog({ title, author, url }, user)
+        const result = await BlogService
+            .createBlog({ title, author, url }, user)
+            .catch((err) => {
+                setError("blog creating failed")
+                setTimeout(() => {
+                    setError(null)
+                }, 3000)
+                return null
+            })
+        if (!result) return
         event.target.title.value = ""
         event.target.author.value = ""
         event.target.url.value = ""
         BlogService.getAll().then(blogs =>
             setBlogs(blogs)
         )
+        setMessage("blog creating successed")
+        setTimeout(() => {
+            setMessage(null)
+        }, 3000)
     }
 
     return (
         <div>
+            <Message message={message} error={error} />
             <AccountForm user={user} loginHandler={loginHandler} logoutHandler={logoutHandler} />
             <CreateForm user={user} createHandler={createHandler} />
             <Blogs user={user} blogs={blogs} />
