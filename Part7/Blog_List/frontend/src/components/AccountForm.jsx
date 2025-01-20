fe2cc0ec76cde990884d1f312996d6f5b8794fb2
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-01-20 17:12:08 -0300

    Part 7 Automatic Code Formatting

---
+++ Part7/Blog_List/frontend/src/components/AccountForm.jsx
@@ -0,0 +1,75 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+import AccountService from '../services/accountService'
+
+const AccountForm = ({ user, setUser, setError, setMessage }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const loginHandler = (event) => {
+        event.preventDefault()
+        AccountService.login(username, password)
+            .then((newUser) => {
+                setUser(newUser)
+                window.localStorage.setItem('user', JSON.stringify(newUser))
+                setMessage('login successful')
+                setUsername('')
+                setPassword('')
+            })
+            .catch((err) => {
+                setError('login unsuccessful')
+            })
+    }
+
+    const logoutHandler = () => {
+        setUser({})
+        window.localStorage.removeItem('user')
+        setMessage('logout successful')
+    }
+
+    if (!user.name)
+        return (
+            <>
+                <h2>log in to application</h2>
+                <form onSubmit={loginHandler}>
+                    username:{' '}
+                    <input
+                        onChange={({ target }) => {
+                            setUsername(target.value)
+                        }}
+                        name='username'
+                        type='text'
+                    />
+                    <br />
+                    password:{' '}
+                    <input
+                        onChange={({ target }) => {
+                            setPassword(target.value)
+                        }}
+                        name='password'
+                        type='password'
+                    />
+                    <br />
+                    <button type='submit'>login</button>
+                </form>
+            </>
+        )
+    else
+        return (
+            <div>
+                {user.username} is logged in{' '}
+                <button type='button' onClick={logoutHandler}>
+                    logout
+                </button>
+            </div>
+        )
+}
+
+AccountForm.propType = {
+    user: PropTypes.object.isRequired,
+    setUser: PropTypes.func.isRequired,
+    setError: PropTypes.func.isRequired,
+    setMessage: PropTypes.func.isRequired,
+}
+
+export default AccountForm
