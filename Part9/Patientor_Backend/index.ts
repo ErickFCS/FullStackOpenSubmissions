d207bd72d2dcd9fa583f87ec344049dee97811a3
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-01-31 16:39:02 -0300

    Part 9 Patientor backend step 2

--- Part9/Patientor_Backend/index.ts
+++ Part9/Patientor_Backend/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors())
 
 app.get('/api/ping', (_req, res) => {
     res.send("pong");
 });
 
-const PORT: number = 3003;
+const PORT: number = 3001;
 
 app.listen(PORT, () => { console.log(`server running in port ${PORT}`); });
